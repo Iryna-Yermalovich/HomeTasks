d7d36726b1a32f74448025a4dcdabb51846dc9a5
Iryna Yermalovich <iryna_yermalovicg@epam.com>
2015-11-24 02:02:15 +0300

    Task9 update

--- src/Tasks/JavaTask9.java
+++ src/Tasks/JavaTask9.java
@@ -6,42 +6,46 @@ public class JavaTask9 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double[] firstMas = new double[2];
-		double[] secondMas = new double[3];
+		int n = 3, m = 2;
+		double[] a = new double[10];
+		double[] b = new double[10];
 		int k;
 		while (true) {
 			try {
-				System.out.println("Enter two numbers: ");
-				for (int i = 0; i < firstMas.length; i++) {
+				System.out.println("Enter three numbers: ");
+				for (int i = 0; i < n; i++) {
 					if (sc.hasNextDouble()) {
-						firstMas[i] = Double.parseDouble(sc.nextLine());
+						a[i] = Double.parseDouble(sc.nextLine());
 					}
 				}
-				System.out.println("Enter three numbers: ");
-				for (int i = 0; i < secondMas.length; i++) {
+				System.out.println("Enter two numbers: ");
+				for (int i = 0; i < m; i++) {
 					if (sc.hasNextDouble()) {
-						secondMas[i] = Double.parseDouble(sc.nextLine());
+						b[i] = Double.parseDouble(sc.nextLine());
 					}
 				}
 				System.out.println("Enter k: ");
 				k = Integer.parseInt(sc.nextLine());
-
+				if (k > n) {
+					System.err.println("Incorrect k!");
+				}
 				break;
 			} catch (NumberFormatException e) {
 				System.out.println("Incorrect number!\n");
 				continue;
 			}
 		}
-		for (int i = firstMas.length; i < (firstMas.length + secondMas.length); i++) {
-			//firstMas[] = new double[firstMas.length + secondMas.length];
-			firstMas[k + i] = firstMas[i];
+		for (int i = k; i < n + k; i++) {
+			a[i + m] = a[i];
 		}
-		for (int i = 0; i < firstMas.length ; i++) {
-			System.out.println(firstMas.length + " " + firstMas[i]);
+		for (int i = 0; i < m; i++) {
+			a[i + k] = b[i];
+		}
+
+		for (int i = 0; i < m + n; i++) {
+			System.out.println(a[i]);
 		}
 		
 		sc.close();
-
 	}
-
 }
