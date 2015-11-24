27df238a13c5d722aa61a6d2e80afee08fbfaf91
Iryna Yermalovich <Iryna_Yermalovich@epam.com>
2015-11-24 12:48:51 +0300

    Task 9 modifaed

--- src/Tasks/JavaTask9.java
+++ src/Tasks/JavaTask9.java
@@ -4,48 +4,54 @@ import java.util.Scanner;
 
 public class JavaTask9 {
 
+	private static final int N = 5;
+	private static final int M = 6;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = 3, m = 2;
-		double[] a = new double[10];
-		double[] b = new double[10];
+		int[] a = new int[N];
+		int[] b = new int[M];
+		int[] c = new int[N + M];
 		int k;
-		while (true) {
-			try {
-				System.out.println("Enter three numbers: ");
-				for (int i = 0; i < n; i++) {
-					if (sc.hasNextDouble()) {
-						a[i] = Double.parseDouble(sc.nextLine());
-					}
-				}
-				System.out.println("Enter two numbers: ");
-				for (int i = 0; i < m; i++) {
-					if (sc.hasNextDouble()) {
-						b[i] = Double.parseDouble(sc.nextLine());
-					}
-				}
-				System.out.println("Enter k: ");
-				k = Integer.parseInt(sc.nextLine());
-				if (k > n) {
-					System.err.println("Incorrect k!");
-				}
-				break;
-			} catch (NumberFormatException e) {
-				System.out.println("Incorrect number!\n");
-				continue;
-			}
+		for (int i = 0; i < N; i++) {
+			a[i] = (int) (Math.random() * 12);
 		}
-		for (int i = k; i < n + k; i++) {
-			a[i + m] = a[i];
+		for (int i = 0; i < M; i++) {
+			b[i] = (int) (Math.random() * 12);
 		}
-		for (int i = 0; i < m; i++) {
-			a[i + k] = b[i];
+		System.out.println("Enter k: ");
+		k = Integer.parseInt(sc.nextLine());
+		if (k > N) {
+			System.err.println("Incorrect k!");
 		}
 
-		for (int i = 0; i < m + n; i++) {
-			System.out.println(a[i]);
+		for (int i = 0; i < c.length; i++) {
+			if (i < k) {
+				c[i] = a[i];
+			}
+			if (i == k) {
+				for (int j = 0; j < b.length; j++) {
+					c[j + i] = b[j];
+				}
+			}
+			for (int j = k; j < a.length; j++) {
+				c[j + b.length] = a[j];
+			}
 		}
-		
+
+		for (int i = 0; i < N; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < M; i++) {
+			System.out.print(b[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < M + N; i++) {
+			System.out.print(c[i] + " ");
+		}
+
 		sc.close();
 	}
+
 }
